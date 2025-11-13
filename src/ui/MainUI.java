09ce90998a7eb7db2fb34c7f72e5d2e67f88853e
SharpKean <104880312+SharpKean@users.noreply.github.com>
2025-11-13 11:56:35 +0800

    Update 2025.11.13

--- src/ui/MainUI.java
+++ src/ui/MainUI.java
@@ -36,6 +36,7 @@ public class MainUI {
 
         jPanelMain.add(verticalSplitPane, BorderLayout.CENTER);
         root = jPanelMain;
+
         JPanel upPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         String[] searchFields = {"Remarks", "ID", "Url", "Port", "Method", "Route", "Request", "Response"};
         JComboBox<String> fieldComboBox = new JComboBox<>(searchFields);
@@ -43,33 +44,41 @@ public class MainUI {
         JButton searchButton = new JButton("Search");
         JButton importButton = new JButton("导入路径");
         filePathLabel = new JLabel("请导入数据库db文件");
+
         upPanel.add(fieldComboBox);
         upPanel.add(searchField);
         upPanel.add(searchButton);
         upPanel.add(importButton);
         upPanel.add(filePathLabel);
+
         String[] columnNames = {"ID", "Url", "Port", "Method", "Route", "Remarks"};
         DefaultTableModel model = new DefaultTableModel(columnNames, 0);
         table = new JTable(model);
         TableRowSorter<DefaultTableModel> sorter = new TableRowSorter<>(model);
         table.setRowSorter(sorter);
         JScrollPane scrollPane = new JScrollPane(table);
+
         leftPanel.add(upPanel, BorderLayout.NORTH);
         leftPanel.add(scrollPane, BorderLayout.CENTER);
+
         JPopupMenu popupMenu = new JPopupMenu();
+
         JMenuItem main_sendToRepeaterItem = new JMenuItem("Send to Repeater");
         JMenuItem main_copyUrlItem = new JMenuItem("复制 URL");
         JMenuItem main_deleteItem = new JMenuItem("删除数据");
         popupMenu.add(main_sendToRepeaterItem);
         popupMenu.add(main_copyUrlItem);
         popupMenu.add(main_deleteItem);
+
         table.setComponentPopupMenu(popupMenu);
-        table.getColumnModel().getColumn(0).setPreferredWidth(60);   // ID
-        table.getColumnModel().getColumn(1).setPreferredWidth(300);  // Url
-        table.getColumnModel().getColumn(2).setPreferredWidth(80);   // Port
-        table.getColumnModel().getColumn(3).setPreferredWidth(80);   // Method
-        table.getColumnModel().getColumn(4).setPreferredWidth(400);  // Route
-        table.getColumnModel().getColumn(5).setPreferredWidth(500);  // Remarks
+
+        table.getColumnModel().getColumn(0).setPreferredWidth(60);
+        table.getColumnModel().getColumn(1).setPreferredWidth(300);
+        table.getColumnModel().getColumn(2).setPreferredWidth(80);
+        table.getColumnModel().getColumn(3).setPreferredWidth(80);
+        table.getColumnModel().getColumn(4).setPreferredWidth(400);
+        table.getColumnModel().getColumn(5).setPreferredWidth(500);
+
         SwingUtilities.invokeLater(() -> {
             Config.createConfigFile();
             String dbFile = Config.getDBFile("db_file");
@@ -78,16 +87,19 @@ public class MainUI {
             String threadNum = Config.getDBFile("thread_num");
             String typeScan = Config.getDBFile("type_scan");
             String xssEnable = Config.getDBFile("xss_enable");
+
             if (!dbFile.equals("null")) {
                 filePathLabel.setText("文件路径：" + dbFile);
                 loadDataFromDB(dbFile, "remarks", null);
             }
+
             if (!dirFile.equals("null")) {
                 rightPanel.setPathFieldText(dirFile);
                 rightPanel.readTxtFileAndStoreContent(dirFile);
             } else {
                 rightPanel.setPathFieldText("请导入txt字典【注：必须为txt文件】");
             }
+
             if (!filterUrl.equals("null")) {
                 rightPanel.setfilterField(filterUrl);
             }
@@ -101,11 +113,13 @@ public class MainUI {
                 rightPanel.setXssField(xssEnable);
             }
         });
+
         searchButton.addActionListener(e -> {
             String searchText = searchField.getText();
             String selectedField = (String) fieldComboBox.getSelectedItem();
             performSearch(searchText, selectedField);
         });
+
         importButton.addActionListener(e -> {
             JFileChooser fc = new JFileChooser();
             int result = fc.showOpenDialog(root);
@@ -117,6 +131,7 @@ public class MainUI {
                 Config.updateConfig(path, null, null, null, null, null);
             }
         });
+
         main_deleteItem.addActionListener(e -> {
             int[] viewRows = table.getSelectedRows();
             if (viewRows.length == 0) {
@@ -140,6 +155,7 @@ public class MainUI {
                 deleteDataFromDB(id);
             }
         });
+
         main_copyUrlItem.addActionListener(e -> {
             int[] selectedRows = table.getSelectedRows();
             StringBuilder sb = new StringBuilder();
@@ -155,6 +171,7 @@ public class MainUI {
                 clipboard.setContents(selection, null);
             }
         });
+
         main_sendToRepeaterItem.addActionListener(e -> {
             int[] selectedRows = table.getSelectedRows();
             if (selectedRows.length == 0) {
@@ -163,16 +180,18 @@ public class MainUI {
             }
             for (int row : selectedRows) {
                 int modelRow = table.convertRowIndexToModel(row);
-                // 获取表格数据
                 int id = (int) model.getValueAt(modelRow, 0);
                 String urlStr = (String) model.getValueAt(modelRow, 1);
                 String portStr = (String) model.getValueAt(modelRow, 2);
                 int port = Integer.parseInt(portStr);
+
                 String sql_request = queryRequestAndResponse(id, 2);
+
                 if (sql_request == null || sql_request.isEmpty()) {
                     JOptionPane.showMessageDialog(root, "（ID: " + id + "）的请求包为空", "温馨提示", JOptionPane.WARNING_MESSAGE);
                     continue;
                 }
+
                 byte[] requestBytes;
                 try {
                     requestBytes = sql_request.getBytes(java.nio.charset.StandardCharsets.UTF_8);
@@ -181,8 +200,10 @@ public class MainUI {
                     ex.printStackTrace();
                     continue;
                 }
+
                 String host;
                 boolean useHttps = false;
+
                 if (urlStr.startsWith("https://")) {
                     useHttps = true;
                     host = urlStr.substring(8);
@@ -199,6 +220,7 @@ public class MainUI {
                 if (colonIndex != -1) {
                     host = host.substring(0, colonIndex);
                 }
+
                 try {
                     BurpExtender.callbacks.sendToRepeater(
                             host,
@@ -213,33 +235,40 @@ public class MainUI {
                 }
             }
         });
+
         model.addTableModelListener(e -> {
             if (e.getType() == TableModelEvent.UPDATE) {
                 int row = e.getFirstRow();
                 int col = e.getColumn();
                 String value = model.getValueAt(row, col).toString();
+
                 int id = Integer.parseInt(model.getValueAt(row, 0).toString());
                 String columnName = model.getColumnName(col);
+
                 String dbColumn = mapDisplayToDBColumn(columnName);
+
                 updateDataInDB(id, dbColumn, value);
             }
         });
+
         table.getSelectionModel().addListSelectionListener(e -> {
             if (!e.getValueIsAdjusting()) {
                 int selectedRow = table.getSelectedRow();
                 if (selectedRow != -1) {
                     int id = (int) table.getValueAt(selectedRow, 0);
-                    queryRequestAndResponse(id,1);
+                    queryRequestAndResponse(id, 1);
                 }
             }
         });
     }
+
     private void performSearch(String searchText, String selectedField) {
         String filePath = getFilePath();
         if (filePath == null || filePath.isEmpty()) {
             JOptionPane.showMessageDialog(root, "请先导入数据库文件", "提示", JOptionPane.WARNING_MESSAGE);
             return;
         }
+
         if ("Request".equals(selectedField) || "Response".equals(selectedField)) {
             String actualColumn = mapDisplayToDBColumn(selectedField);
             loadSpecialFieldFromDB(filePath, actualColumn, searchText);
@@ -247,15 +276,20 @@ public class MainUI {
             String actualColumn = mapDisplayToDBColumn(selectedField);
             loadDataFromDB(filePath, actualColumn, searchText);
         }
-    }public void loadSpecialFieldFromDB(String filePath, String column, String searchText) {
+    }
+
+    public void loadSpecialFieldFromDB(String filePath, String column, String searchText) {
         List<Object[]> dataList = new ArrayList<>();
         try {
             Class.forName("org.sqlite.JDBC");
             String url = "jdbc:sqlite:" + filePath;
             String sql = "SELECT id, url, port, method, route, remarks FROM TLA WHERE " + column + " LIKE ?";
+
             try (Connection connection = DriverManager.getConnection(url);
                  PreparedStatement statement = connection.prepareStatement(sql)) {
+
                 statement.setString(1, "%" + searchText + "%");
+
                 try (ResultSet rs = statement.executeQuery()) {
                     while (rs.next()) {
                         Object[] row = new Object[6];
@@ -275,6 +309,7 @@ public class MainUI {
             JOptionPane.showMessageDialog(root, "数据库查询出错：" + e.getMessage(), "错误", JOptionPane.ERROR_MESSAGE);
         }
     }
+
     public void loadDataFromDB(String filePath, String column, String searchText) {
         List<Object[]> dataList = new ArrayList<>();
         try {
@@ -284,12 +319,15 @@ public class MainUI {
 
             if (searchText != null && !searchText.isEmpty()) {
                 sql += " WHERE " + column + " LIKE ?";
-            }try (Connection connection = DriverManager.getConnection(url);
+            }
+
+            try (Connection connection = DriverManager.getConnection(url);
                  PreparedStatement statement = connection.prepareStatement(sql)) {
 
                 if (searchText != null && !searchText.isEmpty()) {
                     statement.setString(1, "%" + searchText + "%");
                 }
+
                 try (ResultSet rs = statement.executeQuery()) {
                     while (rs.next()) {
                         Object[] row = new Object[6];
@@ -308,23 +346,27 @@ public class MainUI {
             e.printStackTrace();
         }
     }
+
     public void updateTableData(List<Object[]> dataList) {
         DefaultTableModel model = (DefaultTableModel) table.getModel();
         model.setRowCount(0);
         for (Object[] row : dataList) {
             model.addRow(row);
         }
+
         TableRowSorter<DefaultTableModel> sorter = (TableRowSorter<DefaultTableModel>) table.getRowSorter();
         if (sorter == null) {
             sorter = new TableRowSorter<>(model);
             table.setRowSorter(sorter);
         }
+
         sorter.setComparator(0, (o1, o2) -> {
             Integer id1 = Integer.parseInt(o1.toString());
             Integer id2 = Integer.parseInt(o2.toString());
             return id1.compareTo(id2);
         });
     }
+
     public String getFilePath() {
         String text = filePathLabel.getText();
         if (text.startsWith("文件路径：")) {
@@ -332,6 +374,7 @@ public class MainUI {
         }
         return null;
     }
+
     public void insertDataToDatabase(String url, int port, String method, String route, String request, String response, String remarks) {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -353,6 +396,7 @@ public class MainUI {
             e.printStackTrace();
         }
     }
+
     public void deleteDataFromDB(int id) {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -367,6 +411,7 @@ public class MainUI {
             e.printStackTrace();
         }
     }
+
     public void updateDataInDB(int id, String column, String value) {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -382,6 +427,7 @@ public class MainUI {
             e.printStackTrace();
         }
     }
+
     public String queryRequestAndResponse(int id, int type_mode) {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -395,14 +441,12 @@ public class MainUI {
                 if (rs.next()) {
                     String req = rs.getString("request");
                     String res = rs.getString("response");
-                    // 如果 mode 为 1，则更新 UI
                     if (type_mode == 1) {
                         SwingUtilities.invokeLater(() -> {
                             LeftDownUI.setEditorMessage(LeftDownUI.getLeftEditor(), req);
                             LeftDownUI.setEditorMessage(LeftDownUI.getRightEditor(), res);
                         });
-                    }
-                    else if (type_mode == 2) {
+                    } else if (type_mode == 2) {
                         return req;
                     }
                 }
@@ -410,9 +454,9 @@ public class MainUI {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        // 如果没有查到或出错，返回 null
         return null;
     }
+
     private String mapDisplayToDBColumn(String displayColumn) {
         switch (displayColumn) {
             case "ID": return "id";
@@ -425,21 +469,25 @@ public class MainUI {
             default: return "remarks";
         }
     }
+
     private JSplitPane createSplitPane(int orientation, JComponent left, JComponent right, double resizeWeight) {
         JSplitPane sp = new JSplitPane(orientation, left, right);
         sp.setResizeWeight(resizeWeight);
         return sp;
     }
+
     public RightUI getRightPanel() {
         return rightPanel;
     }
+
     public RightDownUI getRightDownPanel() {
         return rightDownPanel;
     }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("MainUI");
-            ui.MainUI mainUI = new ui.MainUI();
+            MainUI mainUI = new MainUI();
             frame.setContentPane(mainUI.root);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(1200, 800);
